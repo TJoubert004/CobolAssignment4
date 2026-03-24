7b6c2e943062e98dbf23b7813157d620a58ddd4b
Tristan Joubert <156536371+TJoubert004@users.noreply.github.com>
2026-03-23 21:23:44 -0500

    fixed alignment errors on output
    fixed alignment errors on output

--- RPT3000.cbl
+++ RPT3000.cbl
@@ -85,38 +85,38 @@
            05  HL1-DAY         PIC 9(2).
            05  FILLER          PIC X(1)    VALUE "/".
            05  HL1-YEAR        PIC 9(4).
-           05  FILLER          PIC X(16)   VALUE SPACE.
+           05  FILLER          PIC X(22)   VALUE SPACE.
            05  FILLER     PIC X(25)   VALUE "YEAR-TO-DATE SALES REPORT".
            05  FILLER          PIC X(19)   VALUE "           PAGE: ".
            05  HL1-PAGE-NUMBER PIC ZZZ9.
-           05  FILLER          PIC X(55)   VALUE SPACE.
+           05  FILLER          PIC X(49)   VALUE SPACE.
 
        01  HEADING-LINE-2.
            05  FILLER          PIC X(7)    VALUE "TIME:  ".
            05  HL2-HOURS       PIC 9(2).
            05  FILLER          PIC X(1)    VALUE ":".
            05  HL2-MINUTES     PIC 9(2).
-           05  FILLER          PIC X(57)   VALUE SPACE.
+           05  FILLER          PIC X(63)   VALUE SPACE.
            05  FILLER          PIC X(10)   VALUE "RPT3000".
-           05  FILLER          PIC X(45)   VALUE SPACE.
+           05  FILLER          PIC X(36)   VALUE SPACE.
 
        01  HEADING-LINE-3.
            05  FILLER PIC X(8) VALUE "BRANCH  ".
-           05  FILLER PIC X(20) VALUE "BRANCH SALES CUST   ".
-           05  FILLER PIC X(23) VALUE "SALES                  ".
+           05  FILLER PIC X(31) VALUE "CUST  ".
+           05  FILLER PIC X(14) VALUE "SALES       ".
            05  FILLER PIC X(14) VALUE "SALES    ".
-           05  FILLER PIC X(14) VALUE "CHANGE        ".
+           05  FILLER PIC X(14) VALUE "CHANGE      ".
            05  FILLER PIC X(7)  VALUE "CHANGE ".
-           05  FILLER PIC X(44) VALUE SPACE.
+           05  FILLER PIC X(52) VALUE SPACE.
 
        01  HEADING-LINE-4.
-           05  FILLER PIC X(8) VALUE "NUM    ".
-           05  FILLER PIC X(23) VALUE "CUSTOMER NAME          ".
-           05  FILLER PIC X(14) VALUE "THIS YTD      ".
+           05  FILLER PIC X(8) VALUE " NUM    ".
+           05  FILLER PIC X(31) VALUE "NUM    CUSTOMER NAME".
+           05  FILLER PIC X(14) VALUE "THIS YTD    ".
            05  FILLER PIC X(14) VALUE "LAST YTD      ".
-           05  FILLER PIC X(13) VALUE "AMOUNT       ".
+           05  FILLER PIC X(14) VALUE "AMOUNT      ".
            05  FILLER PIC X(7)  VALUE "PERCENT".
-           05  FILLER PIC X(44) VALUE SPACE.
+           05  FILLER PIC X(39) VALUE SPACE.
 
        01  HEADING-LINE-5.
            05  FILLER PIC X(6)  VALUE ALL "-".
@@ -140,8 +140,6 @@
            05  FILLER              PIC X(2)    VALUE SPACE.
            05  CL-BRANCH-NUMBER    PIC X(2).
            05  FILLER              PIC X(4)    VALUE SPACE.
-           05  CL-SALESREP-NUMBER  PIC X(2).
-           05  FILLER              PIC X(4)    VALUE SPACE.
            05  CL-CUSTOMER-NUMBER  PIC 9(5).
            05  FILLER              PIC X(2)    VALUE SPACE.
            05  CL-CUSTOMER-NAME    PIC X(20).
@@ -153,34 +151,34 @@
            05  CL-CHANGE-AMOUNT    PIC ZZ,ZZ9.99-.
            05  FILLER              PIC X(4)    VALUE SPACE.
            05  CL-CHANGE-PERCENT   PIC ZZ9.9-.
-           05  FILLER              PIC X(47)   VALUE SPACE.
+           05  FILLER              PIC X(53)   VALUE SPACE.
 
        01  BRANCH-TOTAL-LINE.
            05  FILLER              PIC X(23)   VALUE SPACE.
-           05  FILLER              PIC X(14)   VALUE "BRANCH TOTAL".
+           05  FILLER              PIC X(15)   VALUE "BRANCH TOTAL".
            05  BTL-SALES-THIS-YTD  PIC ZZZ,ZZ9.99-.
            05  FILLER              PIC X(3)    VALUE SPACE.
            05  BTL-SALES-LAST-YTD  PIC ZZZ,ZZ9.99-.
            05  FILLER              PIC X(3)    VALUE SPACE.
            05  BTL-CHANGE-AMOUNT   PIC ZZZ,ZZ9.99-.
-           05  FILLER              PIC X(3)    VALUE SPACE.
+           05  FILLER              PIC X(4)    VALUE SPACE.
            05  BTL-CHANGE-PERCENT  PIC ZZ9.9-.
-           05  FILLER              PIC X(47)   VALUE " *".
+           05  FILLER              PIC X(45)   VALUE "*".
 
        01  GRAND-TOTAL-LINE-1.
-           05  FILLER              PIC X(23)   VALUE SPACE.
-           05  FILLER              PIC X(12)   VALUE "GRAND TOTAL".
+           05  FILLER              PIC X(24)   VALUE SPACE.
+           05  FILLER              PIC X(14)   VALUE "GRAND TOTAL".
            05  FILLER              PIC X(10)   VALUE ALL "=".
            05  FILLER              PIC X(4)    VALUE SPACE.
            05  FILLER              PIC X(10)   VALUE ALL "=".
-           05  FILLER              PIC X(4)    VALUE SPACE.
+           05  FILLER              PIC X(5)    VALUE SPACE.
            05  FILLER              PIC X(10)   VALUE ALL "=".
            05  FILLER              PIC X(3)    VALUE SPACE.
            05  FILLER              PIC X(7)    VALUE ALL "=".
-           05  FILLER              PIC X(47)   VALUE " **".
+           05  FILLER              PIC X(45)   VALUE "**".
 
        01  GRAND-TOTAL-LINE-2.
-           05  FILLER              PIC X(41)   VALUE SPACE.
+           05  FILLER              PIC X(36)   VALUE SPACE.
            05  GTL-SALES-THIS-YTD  PIC Z,ZZZ,ZZ9.99-.
            05  FILLER              PIC X(1)    VALUE SPACE.
            05  GTL-SALES-LAST-YTD  PIC Z,ZZZ,ZZ9.99-.
@@ -188,7 +186,7 @@
            05  GTL-CHANGE-AMOUNT   PIC Z,ZZZ,ZZ9.99-.
            05  FILLER              PIC X(4)    VALUE SPACE.
            05  GTL-CHANGE-PERCENT  PIC ZZ9.9-.
-           05  FILLER              PIC X(37)   VALUE SPACE.
+           05  FILLER              PIC X(42)   VALUE SPACE.
 
        PROCEDURE DIVISION.
        000-PREPARE-SALES-REPORT.
@@ -243,7 +241,6 @@
               ELSE
                  MOVE SPACE TO CL-BRANCH-NUMBER.
 
-           MOVE CM-SALESREP-NUMBER TO CL-SALESREP-NUMBER.
            MOVE CM-CUSTOMER-NUMBER TO CL-CUSTOMER-NUMBER.
            MOVE CM-CUSTOMER-NAME   TO CL-CUSTOMER-NAME.
            MOVE CM-SALES-THIS-YTD  TO CL-SALES-THIS-YTD.
